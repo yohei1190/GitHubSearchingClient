80f2d17672652e7f5b769839479d14927eeb0d3d
yohei shimizu <yohei.shimizu.1190@gmail.com>
2023-10-26 07:52:27 +0900

    Initial Commit

---
+++ GitHubSearchingClient/GitHubSearchingClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSearchingClientApp.swift
+//  GitHubSearchingClient
+//
+//  Created by yohei shimizu on 2023/10/26.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSearchingClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
